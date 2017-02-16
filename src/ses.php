895482619dc7c510786659ca11fc45cb37cac539
okamos <himinato.k@gmail.com>
2017-02-16 23:01:04 +0900

    :golf: Improve code style

--- src/ses.php
+++ src/ses.php
@@ -1,244 +1,330 @@
 <?php
-/*
- * Amazon SimpleEmailService class for PHP
- * Author: Okamos
+/**
+ * SimpleEmailService Class Doc Comment.
  *
+ * Amazon SimpleEmailService for PHP.
+ *
+ * @category Class
+ * @package  AmazonSimpleEmailService
+ * @author   Okamos <okamoto@okamos.com>
+ * @license  https://opensource.org/licenses/MIT MIT License
+ * @link     https://github.com/okamos/php-ses
  */
-class SimpleEmailService {
-  const SERVICE = 'email';
-  const DOMAIN = 'amazonaws.com';
-  const ALGORITHM = 'AWS4-HMAC-SHA256';
-  const ERROR = "Please check your aws_access_key_id and aws_secret_access_key.\nAnd set correct permissions to your user or group.";
+class SimpleEmailService
+{
+    const SERVICE = 'email';
+    const DOMAIN = 'amazonaws.com';
+    const ALGORITHM = 'AWS4-HMAC-SHA256';
+    const ERROR = "Please check your aws_access_key_id and aws_secret_access_key.\nAnd set correct permissions to your user or group.";
 
-  private $aws_key;
-  private $aws_secret;
-  private $region;
+    private $_aws_key;
+    private $_aws_secret;
+    private $_region;
 
-  private $host;
-  private $endpoint;
+    private $_host;
+    private $_endpoint;
 
-  private $amz_date;
-  private $date;
+    private $_amz_date;
+    private $_date;
 
-  private $action;
-  private $method;
+    private $_action;
+    private $_method;
 
-  private $headers;
-  private $query_parameters;
+    private $_headers;
+    private $_query_parameters;
 
-  // aws_access_key_id and aws_secret_access_key is reuqired.
-  // Defaults verification of SSL certificate used.
-  public function __construct($credentials = array(), $ssl_verify_peer = true) {
-    $this -> aws_key = $credentials['aws_access_key_id'];
-    $this -> aws_secret = $credentials['aws_secret_access_key'];
-    // default is us-east-1
-    $this -> region = $credentials['region'] ? $credentials['region'] : 'us-east-1';
+    /**
+     * It is required aws_access_key_id and aws_secret_access_key.
+     * Defaults verification of SSL certificate used.
+     *
+     * @param array   $credentials     It remains aws access_key_id and
+     *                                 aws_secret_access_key, region.
+     * @param boolean $ssl_verify_peer Require verification of peer name.
+     */
+    public function __construct($credentials = array(), $ssl_verify_peer = true)
+    {
+        $this -> _aws_key = $credentials['aws_access_key_id'];
+        $this -> _aws_secret = $credentials['aws_secret_access_key'];
+        // default is us-east-1
+        $this -> _region = $credentials['region'] ? $credentials['region'] : 'us-east-1';
 
-    $this -> host = self::SERVICE . '.' . $this -> region . '.' . self::DOMAIN;
-    $this -> endpoint = 'https://' . self::SERVICE . '.' . $this -> region . '.' . self::DOMAIN;
+        $this -> _host = self::SERVICE . '.' . $this -> _region . '.' . self::DOMAIN;
+        $this -> _endpoint = 'https://' . self::SERVICE . '.' . $this -> _region . '.' . self::DOMAIN;
 
-    $this -> amz_date = gmdate('Ymd\THis\Z');
-    $this -> date = gmdate('Ymd');
+        $this -> _amz_date = gmdate('Ymd\THis\Z');
+        $this -> _date = gmdate('Ymd');
 
-    $this -> ssl_verify = $ssl_verify_peer;
-  }
-
-  // List all identities your AWS account.
-  public function list_identities($identity_type = '') {
-    $this -> action = 'ListIdentities';
-    $this -> method = 'GET';
-
-    if (!preg_match('/^(EmailAddress|Domain|)$/', $identity_type)) {
-      throw new Exception('IdentityType must be EmailAddress or Domain');
-      return;
+        $this -> ssl_verify = $ssl_verify_peer;
     }
 
-    $parameters = array();
-    if ($identity_type) {
-      $parameters['IdentityType'] = $identity_type;
+    /**
+     * List all identities your AWS account.
+     *
+     * @param string $identity_type The type of the identities to list. Possible
+     *                              values are "EmailAddress" and "Domain". If this 
+     *                              parameter is omitted, then all identities will 
+     *                              be listed.
+     *
+     * @return void
+     */
+    public function listIdentities($identity_type = '')
+    {
+        $this -> _action = 'ListIdentities';
+        $this -> _method = 'GET';
+
+        if (!preg_match('/^(EmailAddress|Domain|)$/', $identity_type)) {
+            throw new Exception('IdentityType must be EmailAddress or Domain');
+            return;
+        }
+
+        $parameters = array();
+        if ($identity_type) {
+            $parameters['IdentityType'] = $identity_type;
+        }
+
+        $this -> _generateSignature($parameters);
+        $context = $this -> _createStreamContext();
+        if ($res = @file_get_contents($this -> _endpoint . '?' . $this -> _query_parameters, false, $context)) {
+            $identities = new SimpleXMLElement($res);
+            return $identities -> ListIdentitiesResult -> Identities -> member;
+        }
+        throw new Exception(self::ERROR);
     }
 
-    $this -> generate_signature($parameters);
-    $context = $this -> create_stream_context();
-    if ($res = @file_get_contents($this -> endpoint . '?' . $this -> query_parameters, false, $context)) {
-      $identities = new SimpleXMLElement($res);
-      return $identities -> ListIdentitiesResult -> Identities -> member;
-    }
-    throw new Exception(self::ERROR);
-  }
+    /**
+     * Send an confirmation email to email address for verification.
+     *
+     * @param string $email The email address to be verified.
+     *
+     * @return string;
+     */
+    public function verifyEmailIdentity($email)
+    {
+        $this -> _action = 'VerifyEmailIdentity';
+        $this -> _method = 'GET';
 
-  // Send an confirmation email to email address for verification.
-  public function verify_email_identity($email) {
-    $this -> action = 'VerifyEmailIdentity';
-    $this -> method = 'GET';
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new Exception('Invalid email');
+            return;
+        }
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      throw new Exception('Invalid email');
-      return;
-    }
+        $parameters = array(
+            'EmailAddress' => $email
+        );
 
-    $parameters = array(
-      'EmailAddress' => $email
-    );
-
-    $this -> generate_signature($parameters);
-    $context = $this -> create_stream_context();
-    if ($res = @file_get_contents($this -> endpoint . '?' . $this -> query_parameters, false, $context)) {
-      $xml = simplexml_load_string($res);
-      return $xml -> ResponseMetadata -> RequestId;
-    }
-    throw new Exception(self::ERROR);
-  }
-
-  // Delete an identity from your AWS account.
-  public function delete_identity($identity) {
-    $this -> action = 'DeleteIdentity';
-    $this -> method = 'GET';
-
-    if (!(filter_var($identity, FILTER_VALIDATE_EMAIL) || preg_match('/^([a-z\d]+(-[a-z\d]+)*\.)+[a-z]{2,}$/', $identity))) {
-      throw new Exception('Identity must be EmailAddress or Domain');
-      return;
+        $this -> _generateSignature($parameters);
+        $context = $this -> _createStreamContext();
+        if ($res = @file_get_contents($this -> _endpoint . '?' . $this -> _query_parameters, false, $context)) {
+            $xml = simplexml_load_string($res);
+            return $xml -> ResponseMetadata -> RequestId;
+        }
+        throw new Exception(self::ERROR);
     }
 
-    $parameters = array(
-      'Identity' => $identity
-    );
+    /**
+     * Delete an identity from your AWS account.
+     *
+     * @param string $identity The identity to be removed from the list of identities
+     *                         for the AWS Account. An email address or domain.
+     *
+     * @return string
+     */
+    public function deleteIdentity($identity)
+    {
+        $this -> _action = 'DeleteIdentity';
+        $this -> _method = 'GET';
 
-    $this -> generate_signature($parameters);
-    $context = $this -> create_stream_context();
-    if ($res = @file_get_contents($this -> endpoint . '?' . $this -> query_parameters, false, $context)) {
-      $xml = simplexml_load_string($res);
-      return $xml -> ResponseMetadata -> RequestId;
-    }
-    throw new Exception(self::ERROR);
-  }
+        if (!(filter_var($identity, FILTER_VALIDATE_EMAIL) || preg_match('/^([a-z\d]+(-[a-z\d]+)*\.)+[a-z]{2,}$/', $identity))) {
+            throw new Exception('Identity must be EmailAddress or Domain');
+            return;
+        }
 
-  // TODO: send raw message
-  // Send the email to some specified addresses.
-  public function send_email($assets = array()) {
-    $this -> action = 'SendEmail';
-    $this -> method = 'POST';
+        $parameters = array(
+            'Identity' => $identity
+        );
 
-    $parameters = array(
-      'Message.Body.Text.Data' => $assets['body'],
-      'Message.Subject.Data' => $assets['subject'],
-      'Source' => $assets['from']
-    );
-
-    if (isset($asset['to'])) {
-      $this -> addAddresses($assets['to']);
-    }
-    if (isset($asset['cc'])) {
-      $this -> addAddresses($assets['cc'], 'cc');
-    }
-    if (isset($asset['bcc'])) {
-      $this -> addAddresses($assets['bcc'], 'bcc');
+        $this -> _generateSignature($parameters);
+        $context = $this -> _createStreamContext();
+        if ($res = @file_get_contents($this -> _endpoint . '?' . $this -> _query_parameters, false, $context)) {
+            $xml = simplexml_load_string($res);
+            return $xml -> ResponseMetadata -> RequestId;
+        }
+        throw new Exception(self::ERROR);
     }
 
-    $this -> generate_signature($parameters);
-    $context = $this -> create_stream_context();
-    if ($res = @file_get_contents($this -> endpoint . '?' . $this -> query_parameters, false, $context)) {
-      $xml = simplexml_load_string($res);
-      return $xml -> SendEmailResult -> MessageId;
+    /**
+     * Send the email to some specified addresses.
+     *
+     * @param array $assets "to", "cc", "bcc" are destination for this email.
+     *                      "body" is the message to be sent.
+     *                      "subject" is the subject of the message.
+     *
+     * @return string.
+     *
+     * @todo Send raw message.
+     */
+    public function sendEmail($assets = array())
+    {
+        $this -> _action = 'SendEmail';
+        $this -> _method = 'POST';
+
+        $parameters = array(
+            'Message.Body.Text.Data' => $assets['body'],
+            'Message.Subject.Data' => $assets['subject'],
+            'Source' => $assets['from']
+        );
+
+        if (isset($asset['to'])) {
+            $this -> addAddresses($assets['to']);
+        }
+        if (isset($asset['cc'])) {
+            $this -> addAddresses($assets['cc'], 'cc');
+        }
+        if (isset($asset['bcc'])) {
+            $this -> addAddresses($assets['bcc'], 'bcc');
+        }
+
+        $this -> _generateSignature($parameters);
+        $context = $this -> _createStreamContext();
+        if ($res = @file_get_contents($this -> _endpoint . '?' . $this -> _query_parameters, false, $context)) {
+            $xml = simplexml_load_string($res);
+            return $xml -> SendEmailResult -> MessageId;
+        }
+        throw new Exception(self::ERROR);
     }
-    throw new Exception(self::ERROR);
-  }
 
-  public function addAddresses($addresses, $destination = 'to') {
-    $address_index = 1;
-    if (is_string($addresses)) {
-      $parameters['Destination.' . $destination . 'Addresses.member.' . $address_index] = $addresses;
+    /**
+     * Add To: Cc: Bcc: to the destination for a email.
+     *
+     * @param string[] $addresses   List of emails.
+     * @param string   $destination Destination type. Possible values are "to" and
+     *                              "cc", "bcc".
+     *
+     * @return void
+     */
+    public function addAddresses($addresses, $destination = 'to')
+    {
+        $address_index = 1;
+        if (is_string($addresses)) {
+            $parameters['Destination.' . $destination . 'Addresses.member.' . $address_index] = $addresses;
+        }
+        if (is_array($addresses)) {
+            foreach ($addresses as $address) {
+                $parameters['Destination.' . $destination . 'Addresses.member.' . $address_index] = $address;
+                $address_index++;
+            }
+        }
     }
-    if (is_array($addresses)) {
-      foreach ($addresses as $address) {
-        $parameters['Destination.' . $destination . 'Addresses.member.' . $address_index] = $address;
-        $address_index++;
-      }
+
+    /**
+     * Get your AWS account's sending limits.
+     *
+     * @return double
+     */
+    public function getSendQuota()
+    {
+        $this -> _action = 'GetSendQuota';
+        $this -> _method = 'GET';
+
+        $this -> _generateSignature();
+        $context = $this -> _createStreamContext();
+        if ($res = @file_get_contents($this -> _endpoint . '?' . $this -> _query_parameters, false, $context)) {
+            $xml = simplexml_load_string($res);
+            return $xml -> GetSendQuotaResult;
+        }
+        throw new Exception(self::ERROR);
     }
-  }
 
-  // Get your AWS account's sending limits.
-  public function get_send_quota() {
-    $this -> action = 'GetSendQuota';
-    $this -> method = 'GET';
+    /**
+     * Get SES sending statistics.
+     *
+     * @return array
+     */
+    public function getSendStatistics()
+    {
+        $this -> _action = 'GetSendStatistics';
+        $this -> _method = 'GET';
 
-
-    $this -> generate_signature();
-    $context = $this -> create_stream_context();
-    if ($res = @file_get_contents($this -> endpoint . '?' . $this -> query_parameters, false, $context)) {
-      $xml = simplexml_load_string($res);
-      return $xml -> GetSendQuotaResult;
+        $this -> _generateSignature();
+        $context = $this -> _createStreamContext();
+        if ($res = @file_get_contents($this -> _endpoint . '?' . $this -> _query_parameters, false, $context)) {
+            $xml = simplexml_load_string($res);
+            return $xml -> GetSendStatisticsResult -> SendDataPoints -> member;
+        }
+        throw new Exception(self::ERROR);
     }
-    throw new Exception(self::ERROR);
-  }
 
-  // Get SES sending statistics.
-  public function get_send_statistics() {
-    $this -> action = 'GetSendStatistics';
-    $this -> method = 'GET';
+    /**
+     * Create and returns a stream context.
+     *
+     * @return A stream context resource.
+     */
+    private function _createStreamContext()
+    {
+        $opts = array(
+            'ssl' => array(
+                'verify_peer' => $this -> ssl_verify,
+                'verify_peer_name' => $this -> ssl_verify
+            ),
+            'http' => array(
+                'method' => $this -> _method,
+                'header' => join("\n", $this -> _headers) . "\n"
+            )
+        );
 
-
-    $this -> generate_signature();
-    $context = $this -> create_stream_context();
-    if ($res = @file_get_contents($this -> endpoint . '?' . $this -> query_parameters, false, $context)) {
-      $xml = simplexml_load_string($res);
-      return $xml -> GetSendStatisticsResult -> SendDataPoints -> member;
+        return stream_context_create($opts);
     }
-    throw new Exception(self::ERROR);
-  }
 
-  private function create_stream_context() {
-    $opts = array(
-      'ssl' => array(
-        'verify_peer' => $this -> ssl_verify,
-        'verify_peer_name' => $this -> ssl_verify
-      ),
-      'http' => array(
-        'method' => $this -> method,
-        'header' => join("\n", $this -> headers) . "\n"
-      )
-    );
+    /**
+     * Create and returns binary hmac sha256
+     *
+     * @return hmac sha256.
+     */
+    private function _generateSignatureKey()
+    {
+        $date_h = hash_hmac('sha256', $this -> _date, 'AWS4' . $this -> _aws_secret, true);
+        $region_h = hash_hmac('sha256', $this -> _region, $date_h, true);
+        $service_h = hash_hmac('sha256', self::SERVICE, $region_h, true);
+        $signing_h = hash_hmac('sha256', 'aws4_request', $service_h, true);
 
-    return stream_context_create($opts);
-  }
+        return $signing_h;
+    }
 
-  // return binary hmac sha256
-  private function generate_signature_key() {
-    $date_h = hash_hmac('sha256', $this -> date, 'AWS4' . $this -> aws_secret, true);
-    $region_h = hash_hmac('sha256', $this -> region, $date_h, true);
-    $service_h = hash_hmac('sha256', self::SERVICE, $region_h, true);
-    $signing_h = hash_hmac('sha256', 'aws4_request', $service_h, true);
+    /**
+     * Signing AWS Requests with Signature Version 4
+     *
+     * @param array $parameters It contains request parameters.
+     *
+     * @ref http://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
+     *
+     * @return void
+     */
+    private function _generateSignature($parameters = array())
+    {
+        $canonical_uri = '/';
 
-    return $signing_h;
-  }
+        $parameters['Action'] = $this -> _action;
+        ksort($parameters);
 
-  // Signing AWS Requests with Signature Version 4
-	// see http://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
-  private function generate_signature($parameters = array()) {
-    $canonical_uri = '/';
+        $request_parameters = http_build_query($parameters, '', '&', PHP_QUERY_RFC3986);
 
-    $parameters['Action'] = $this -> action;
-    ksort($parameters);
+        $canonical_headers = 'host:' . $this -> _host . "\n" . 'x-amz-date:' . $this -> _amz_date . "\n";
+        $signed_headers = 'host;x-amz-date';
+        $payload_hash = hash('sha256', '');
 
-    $request_parameters = http_build_query($parameters, '', '&', PHP_QUERY_RFC3986);
+        // task1
+        $canonical_request = $this -> _method . "\n" . $canonical_uri . "\n" . $request_parameters . "\n" . $canonical_headers . "\n" . $signed_headers . "\n" . $payload_hash;
 
-    $canonical_headers = 'host:' . $this -> host . "\n" . 'x-amz-date:' . $this -> amz_date . "\n";
-    $signed_headers = 'host;x-amz-date';
-    $payload_hash = hash('sha256', '');
+        // task2
+        $credential_scope = $this -> _date . '/' . $this -> _region . '/' . self::SERVICE . '/aws4_request';
+        $string_to_sign =  self::ALGORITHM . "\n" . $this -> _amz_date . "\n" . $credential_scope . "\n" . hash('sha256', $canonical_request);
 
-    # task1
-    $canonical_request = $this -> method . "\n" . $canonical_uri . "\n" . $request_parameters . "\n" . $canonical_headers . "\n" . $signed_headers . "\n" . $payload_hash;
-
-    # task2
-    $credential_scope = $this -> date . '/' . $this -> region . '/' . self::SERVICE . '/aws4_request';
-    $string_to_sign =  self::ALGORITHM . "\n" . $this -> amz_date . "\n" . $credential_scope . "\n" . hash('sha256', $canonical_request);
-
-    # task3
-    $signing_key = $this -> generate_signature_key();
-    $signature = hash_hmac('sha256', $string_to_sign, $signing_key);
-    $this -> headers[] = 'Authorization:' . self::ALGORITHM . ' Credential=' . $this -> aws_key . '/' . $credential_scope . ', SignedHeaders=' . $signed_headers . ', Signature=' . $signature;
-    $this -> headers[] = 'x-amz-date:' . $this -> amz_date;
-    $this -> query_parameters = $request_parameters;
-  }
+        // task3
+        $signing_key = $this -> _generateSignatureKey();
+        $signature = hash_hmac('sha256', $string_to_sign, $signing_key);
+        $this -> _headers[] = 'Authorization:' . self::ALGORITHM . ' Credential=' . $this -> _aws_key . '/' . $credential_scope . ', SignedHeaders=' . $signed_headers . ', Signature=' . $signature;
+        $this -> _headers[] = 'x-amz-date:' . $this -> _amz_date;
+        $this -> _query_parameters = $request_parameters;
+    }
 }
 ?>
